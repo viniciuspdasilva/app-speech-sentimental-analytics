a7ab4091821c84c9449f577b82d443752493d4bd
Vinicius Pereira da Silva <viniciussilva32171@gmail.com>
2020-07-01 22:45:16 -0300

    Initial commit

---
+++ build_model.py
@@ -0,0 +1,36 @@
+from model import NLPModel
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def build_model():
+    model = NLPModel()
+
+    with open('../lib/model/train.tsv') as f:
+        data = pd.read_csv(f, sep='\t')
+
+    pos_neg = data[(data['Sentiment'] == 0) | (data['Sentiment'] == 4)]
+
+    pos_neg['Binary'] = pos_neg.apply(
+        lambda x: 0 if x['Sentiment'] == 0 else 1, axis=1)
+
+    model.vectorizer_fit(pos_neg.loc[:, 'Phrase'])
+    print('Vectorizer fit complete')
+
+    X = model.vectorizer_transform(pos_neg.loc[:, 'Phrase'])
+    print('Vectorizer transform complete')
+    y = pos_neg.loc[:, 'Binary']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model.train(X_train, y_train)
+    print('Model training complete')
+
+    model.pickle_clf()
+    model.pickle_vectorizer()
+
+    model.plot_roc(X_test, y_test)
+
+
+if __name__ == "__main__":
+    build_model()
